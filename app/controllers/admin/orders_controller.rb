8dcac644662a72fa3457168893d74ee25ec1665a
13949836783@163.com <13949836783@163.com>
2017-07-01 21:56:52 +0800

    add aasm

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,13 @@
+class Admin::OrdersController < ApplicationController
+before_action :authenticate_user!
+before_action :admin_required
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+end
