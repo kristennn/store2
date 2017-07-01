172b8125f20a28303f9aead0cb595c7f5b4ea17e
13949836783@163.com <13949836783@163.com>
2017-07-01 21:42:25 +0800

    user can see their orders info

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ApplicationRecord
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
+  belongs_to :user
+  has_many :product_lists
+end
